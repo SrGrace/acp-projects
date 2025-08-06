83ca07b50489e8e2fc57c40539ada11eb7f3b706
Sourav Verma <16613596+SrGrace@users.noreply.github.com>
2025-08-06 11:06:30 +0530

    add async client

---
+++ langgraph_greeting/client_async.py
@@ -0,0 +1,18 @@
+import asyncio
+
+from acp_sdk.client import Client
+from acp_sdk.models import (
+    Message,
+    MessagePart,
+)
+
+async def client() -> None:
+    async with Client(base_url="http://localhost:8000") as client:
+        run = await client.run_sync(
+            agent="lang_graph_greeting_agent", input=[Message(parts=[MessagePart(content="John Doe")])]
+        )
+        print(run.output[0].parts[0].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(client())
